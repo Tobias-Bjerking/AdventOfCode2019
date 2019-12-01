36f3154bad9727e8e03456882d3f7689fdd16e70
Tobbe <tobias.bjerking@gmail.com>
2019-12-01 11:33:23 +0100

    refactor the modules loop into its own function

--- DailyChallenges/Day1.py
+++ DailyChallenges/Day1.py
@@ -1,15 +1,16 @@
 import GetInput
 
-def calculate_fuel_consumption(module: int) -> int:
+def calculate_module_fuel_consumption(module: int) -> int:
     fuel = module//3 - 2
     if fuel > 0:
-        return fuel + calculate_fuel_consumption(fuel)
+        return fuel + calculate_module_fuel_consumption(fuel)
     return 0
 
+def calculate_total_fuel_consumption(modules: list) -> int:
+    total_fuel = 0
+    for module in modules:
+        total_fuel += calculate_module_fuel_consumption(module)
+    return total_fuel
 
 modules = GetInput.get_numberd_list("../Input/Day1.txt")
-
-total_fuel = 0
-for module in modules:
-    total_fuel += calculate_fuel_consumption(module)
-print(total_fuel)
+print(calculate_total_fuel_consumption(modules))
