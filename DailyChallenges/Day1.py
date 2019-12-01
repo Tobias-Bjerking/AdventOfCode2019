06b5e3e248ae531662edacab806dfcfeb8cce834
Tobbe <tobias.bjerking@gmail.com>
2019-12-01 11:21:16 +0100

    Day1 part2 completed

--- DailyChallenges/Day1.py
+++ DailyChallenges/Day1.py
@@ -1,7 +1,10 @@
 import GetInput
 
 def calculate_fuel_consumption(module: int) -> int:
-    return module//3 - 2
+    fuel = module//3 - 2
+    if fuel > 0:
+        return fuel + calculate_fuel_consumption(fuel)
+    return 0
 
 
 modules = GetInput.get_numberd_list("../Input/Day1.txt")
