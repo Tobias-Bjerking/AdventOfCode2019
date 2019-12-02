0f05adacb29be6e481285117ecaeaa0522db99a8
Tobbe <tobias.bjerking@gmail.com>
2019-12-02 06:35:51 +0100

    Day 2 part 1 completed

--- DailyChallenges/Day2.py
+++ DailyChallenges/Day2.py
@@ -1,4 +1,24 @@
 import GetInput
 
 
-values = GetInput.get_numbered_list("../Input/Day2.txt")
+def loop(values):
+    operators = values
+    for operator in range(0, len(operators), 4):
+        if operators[operator] == 1:
+            operators[operators[operator + 3]] = operators[operators[operator + 1]] + operators[operators[operator + 2]]
+        elif operators[operator] == 2:
+            operators[operators[operator + 3]] = operators[operators[operator + 1]] * operators[operators[operator + 2]]
+        elif operators[operator] == 99:
+            print("DONE!")
+            return operators
+        else:
+            print("ERROR!")
+    print("loop finished")
+    return operators
+
+
+values = GetInput.get_numbered_line("../Input/Day2.txt")
+values[1] = 12
+values[2] = 2
+test = [1,9,10,3,2,3,11,0,99,30,40,50]
+print(loop(values))
