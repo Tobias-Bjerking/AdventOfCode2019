a9db8c3ac1a668e27fc887ece7a3a95a8a5f1ae3
Tobbe <tobias.bjerking@gmail.com>
2019-12-02 06:35:08 +0100

    Add a function that handles a line of numbers

--- GetInput.py
+++ GetInput.py
@@ -8,3 +8,8 @@ def get_list(path: str) -> list:
     file_input = open(path).read()
     values = list(file_input.splitlines())
     return values
+
+
+def get_numbered_line(path: str) -> list:
+    values = get_list(path)
+    return list(map(int, values[0].split(",")))
