b824cef3190e7138479cca6a91aa9b07b0a20a9d
Tobbe <tobias.bjerking@gmail.com>
2019-12-03 09:13:52 +0100

    Day 3 part 2 complete

--- DailyChallenges/Day3.py
+++ DailyChallenges/Day3.py
@@ -23,16 +23,37 @@ def draw_wire(wires):
     return paths
 
 
-def find_closest_intersection(paths):
-    intersection = paths[0] & paths[1]
+def find_closest_intersection_manhattan(intersections):
     min_dist = sys.maxsize
-    for x in intersection:
+    for x in intersections:
         min_dist = min(min_dist, manhattan_distance((0,0), x))
     return min_dist
 
 
+def find_closest_intersection_length(wires, intersections):
+    dirs = {'R': (0, 1), 'L': (0, -1), 'U': (-1, 0), 'D': (1, 0)}
+    length = {0:{},1:{}}
+    iter = 0
+    for wire in wires:
+        x = y = steps = 0
+        for direction in wire:
+            direct, dist = direction[0], int(direction[1:])
+            dist_x, dist_y = dirs[direct]
+            for i in range(dist):
+                steps += 1
+                x += dist_x
+                y += dist_y
+                length[iter][(x,y)] = steps
+        iter+=1
+
+    min_dist = sys.maxsize
+    for cross in intersections:
+        min_dist = min(min_dist, length[0][cross] + length[1][cross])
+    return min_dist
+
 
 values = GetInput.get_line("../Input/Day3.txt")
 paths = draw_wire(values)
-closest = find_closest_intersection(paths)
-print(closest)
+intersections = paths[0] & paths[1]
+print("Closest intersection manhattan:", find_closest_intersection_manhattan(intersections))
+print("Closest intersection length:",find_closest_intersection_length(values, intersections))
