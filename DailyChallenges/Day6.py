45f37a9a6af9cd52cb9631a0626193ce6b1b3d83
Tobbe <tobias.bjerking@gmail.com>
2019-12-06 08:51:59 +0100

    Day 6 part 2 complete

--- DailyChallenges/Day6.py
+++ DailyChallenges/Day6.py
@@ -12,10 +12,54 @@ def __traverse_tree(node, depth):
     if node not in orbits.keys():
         return depth + 1
     else:
-        satelites_total = 0
-        for satelite in orbits[node]:
-            satelites_total += __traverse_tree(satelite, depth + 1)
-        return satelites_total + depth + 1
+        satellites_total = 0
+        for satellite in orbits[node]:
+            satellites_total += __traverse_tree(satellite, depth + 1)
+        return satellites_total + depth + 1
 
 
-print(get_number_of_orbits())
+def get_orbit(val):
+    for key, value in orbits.items():
+        if val in value:
+            return key
+
+
+def get_all_satellites(node):
+    orbiting_satellites = []
+    if node in orbits.keys():
+        for satellite in orbits[node]:
+            orbiting_satellites.append(satellite)
+            orbitals = get_all_satellites(satellite)
+            for x in orbitals:
+                orbiting_satellites.append(x)
+    return orbiting_satellites
+
+
+def __shared_orbit(node):
+    orbiting = get_orbit(node)
+    satellites = get_all_satellites(orbiting)
+    if "YOU" in satellites and "SAN" in satellites:
+        return orbiting
+    else:
+        return __shared_orbit(orbiting)
+
+def shared_orbit():
+    return __shared_orbit("YOU")
+
+
+def __calculate_distance(node, search):
+    for satellite in orbits[node]:
+        all_satellites = get_all_satellites(satellite)
+        if search in all_satellites:
+            return __calculate_distance(satellite, search) + 1
+    return 0
+
+
+def calculate_distance():
+    shared = shared_orbit()
+    total = __calculate_distance(shared, "YOU")
+    return total + __calculate_distance(shared, "SAN")
+
+
+print("Part 1:", get_number_of_orbits())
+print("Part 2:", calculate_distance())
