bbe56c82486384051df89e5f95f371a3fb4ffaec
Tobbe <tobias.bjerking@gmail.com>
2019-12-06 07:49:00 +0100

    Add function for mapping input in a dictionary

--- GetInput.py
+++ GetInput.py
@@ -10,6 +10,18 @@ def get_list(path: str) -> list:
     return values
 
 
+def get_map(path: str, split: str = ",") -> dict:
+    values = get_list(path)
+    dictionary = {}
+    for string in values:
+        m, v = string.split(split)
+        if m not in dictionary.keys():
+            dictionary[m] = [v]
+        else:
+            dictionary[m].append(v)
+    return dictionary
+
+
 def get_numbered_line(path: str, split: str = ",") -> list:
     values = get_list(path)
     return list(map(int, values[0].split(split)))
