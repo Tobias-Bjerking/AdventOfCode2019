4c5fd031b96ec6f3f9b3f065a557b1aa0cc673d6
Tobbe <tobias.bjerking@gmail.com>
2019-12-12 19:43:21 +0100

    Day 12 part 2 complete

--- DailyChallenges/Day12.py
+++ DailyChallenges/Day12.py
@@ -1,4 +1,7 @@
 import GetInput
+import time
+import winsound
+from math import gcd
 
 values = GetInput.get_list("../Input/Day12.txt")
 moons = []
@@ -46,4 +49,44 @@ def part_one():
         total_energy += pot_energy * kin_energy
     return total_energy
 
+
+def lcm(arr):
+    lcm = arr[0]
+    for i in arr[1:]:
+        lcm = lcm * i // gcd(lcm, i)
+    return lcm
+
+
+def part_two():
+    initialize()
+    initial_state_x = []
+    initial_state_y = []
+    initial_state_z = []
+    for moon in range(len(moons)):
+        initial_state_x.append((moons[moon][0], velocity[moon][0]))
+        initial_state_y.append((moons[moon][1], velocity[moon][1]))
+        initial_state_z.append((moons[moon][2], velocity[moon][2]))
+    steps = x_steps = y_steps = z_steps = 0
+    while x_steps == 0 or y_steps == 0 or z_steps == 0:
+        step()
+        steps += 1
+        new_state_x = []
+        new_state_y = []
+        new_state_z = []
+        for moon in range(len(moons)):
+            new_state_x.append((moons[moon][0], velocity[moon][0]))
+            new_state_y.append((moons[moon][1], velocity[moon][1]))
+            new_state_z.append((moons[moon][2], velocity[moon][2]))
+            if initial_state_x == new_state_x and x_steps == 0:
+                x_steps = steps
+            if initial_state_y == new_state_y and y_steps == 0:
+                y_steps = steps
+            if initial_state_z == new_state_z and z_steps == 0:
+                z_steps = steps
+    return lcm([x_steps, y_steps, z_steps])
+
+
 print("Part 1:", part_one())
+start_time = time.time()
+print("Part 2:", part_two())
+print(time.time() - start_time)
